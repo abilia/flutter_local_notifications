c8815feb7dfd7377bf1b3970dc9ac236930c0862
rickard <rickardeden@gmail.com>
2023-06-14 08:12:56 +0200

    SGC-2475 - check if screen is locked

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
@@ -17,62 +17,62 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 
-/** Created by michaelbui on 24/3/18. */
+/**
+ * Created by michaelbui on 24/3/18.
+ */
 @Keep
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
 
-  private static final String TAG = "ScheduledNotificationReceiver";
+    private static final String TAG = ScheduledNotificationReceiver.class.getName();
 
-  @Override
-  @SuppressWarnings("deprecation")
-  public void onReceive(final Context context, Intent intent) {
-    String notificationDetailsJson =
-        intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
-    if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
-      // This logic is needed for apps that used the plugin prior to 0.3.4
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onReceive(final Context context, Intent intent) {
+        final String notificationDetailsJson =
+                intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
+        if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
+            // This logic is needed for apps that used the plugin prior to 0.3.4
 
-      Notification notification;
-      int notificationId = intent.getIntExtra("notification_id", 0);
+            Notification notification;
+            final int notificationId = intent.getIntExtra("notification_id", 0);
 
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-        notification = intent.getParcelableExtra("notification", Notification.class);
-      } else {
-        notification = intent.getParcelableExtra("notification");
-      }
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                notification = intent.getParcelableExtra("notification", Notification.class);
+            } else {
+                notification = intent.getParcelableExtra("notification");
+            }
 
-      if (notification == null) {
-        // This means the notification is corrupt
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-        Log.e(TAG, "Failed to parse a notification from  Intent. ID: " + notificationId);
-        return;
-      }
+            if (notification == null) {
+                // This means the notification is corrupt
+                FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
+                Log.e(TAG, "Failed to parse a notification from  Intent. ID: " + notificationId);
+                return;
+            }
 
-      notification.when = System.currentTimeMillis();
-      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-      notificationManager.notify(notificationId, notification);
-      boolean repeat = intent.getBooleanExtra("repeat", false);
-      if (!repeat) {
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-      }
-    } else {
-      Gson gson = FlutterLocalNotificationsPlugin.buildGson();
-      Type type = new TypeToken<NotificationDetails>() {}.getType();
-      NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
+            notification.when = System.currentTimeMillis();
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(notificationId, notification);
+            if (!intent.getBooleanExtra("repeat", false)) {
+                FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
+            }
+        } else {
+            final Gson gson = FlutterLocalNotificationsPlugin.buildGson();
+            final Type type = new TypeToken<NotificationDetails>() {}.getType();
+            NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
+            final boolean locked = FlutterLocalNotificationsPlugin.isKeyguardLocked(context);
+            if (notificationDetails.showNotification == null || notificationDetails.showNotification || locked) {
+                FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
+            } else {
+                BackgroundAlarm.start(context, notificationDetails);
+            }
+            FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
 
-      if (notificationDetails.showNotification == null || notificationDetails.showNotification) {
-        FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
-      } else {
-        BackgroundAlarm.start(context, notificationDetails);
-      }
-      FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
-
-      boolean locked = FlutterLocalNotificationsPlugin.isKeyguardLocked(context);
-      boolean firstAlarm =
-          LocalDateTime.parse(notificationDetails.scheduledDateTime).getSecond() == 0;
-      boolean hasStartActivity = notificationDetails.startActivityClassName != null;
-      if (hasStartActivity && (!locked || !firstAlarm)) {
-        FlutterLocalNotificationsPlugin.startAlarmActivity(context, notificationDetails);
-      }
+            final boolean firstAlarm =
+                    LocalDateTime.parse(notificationDetails.scheduledDateTime).getSecond() == 0;
+            final boolean hasStartActivity = notificationDetails.startActivityClassName != null;
+            if (hasStartActivity && (!locked || !firstAlarm)) {
+                FlutterLocalNotificationsPlugin.startAlarmActivity(context, notificationDetails);
+            }
+        }
     }
-  }
 }
